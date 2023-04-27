f0fed9b53ca3efccdd0a4f0853270322a20e1112
wjrcode <willianjunior012@gmail.com>
2023-04-27 11:50:00 -0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: ''
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/tip-calculator-app-main/' : '/'
 })
