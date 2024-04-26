b8db0d3cc2f824c0f4d822722721e73b3cb7386e
Willian Pinheiro Jr <47341070+wjrcode@users.noreply.github.com>
2024-04-26 08:26:16 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <TipCalculator />
     <div class="attribution">
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-      Coded by <a href="#">Your Name Here</a>.
+      Coded by <a href="https://github.com/wjrcode">Willian Pinheiro Jr</a>.
     </div>
   </main>
 </template>
